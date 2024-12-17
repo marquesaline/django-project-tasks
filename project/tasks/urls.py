8e855e5f2ce376fb256254a45199d38f3ba8dc16
Aline Marques <alinem_oliveira@yahoo.com>
2024-12-17 17:13:27 -0300

    updated project

--- project/tasks/urls.py
+++ project/tasks/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('criar/', views.create_task, name='create_task'),
+    path('', views.list_tasks, name='list_tasks'),
+    path('<int:id>/', views.get_task, name='get_task'),
+    path('atualizar/<int:id>/', views.update_task, name='update_task'),
+    path('deletar/<int:id>/', views.delete_task, name='delete_task'),
 
 
 ]
--- project/tasks/views.py
+++ project/tasks/views.py
@@ -1,15 +1,37 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 from .forms import TaskForm
 
-# Create your views here.
 def create_task(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
-            form = TaskForm()
+            return redirect('list_tasks')
     else:
         form = TaskForm()
-    return render(request, 'create_task.html', {'form': form})      
+    return render(request, 'form_task.html', {'form': form})      
 
+def list_tasks(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks.html', {'tasks': tasks })
+
+def get_task(request, id):
+    task = get_object_or_404(Task, id=id)
+    return render(request, 'task.html', {'task': task })
+
+def update_task(request, id):
+    task = get_object_or_404(Task, id=id)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('list_tasks')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'form_task.html', {'form': form})
+
+def delete_task(request, id):
+    task = get_object_or_404(Task, id=id)
+    task.delete()
+    return redirect('list_tasks')
